0f7bb581244a145bf1e3d35895cc007290043127
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-01-12 18:35:57 +0530

    Day7

--- src/Day2/Bufferreader.java
+++ src/Day2/Bufferreader.java
@@ -3,7 +3,7 @@ package Day2;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class BufferReader {
+public class Bufferreader {
 
     public  static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- src/Day2/NPrime.java
+++ src/Day2/NPrime.java
@@ -3,30 +3,30 @@ package Day2;
 public class NPrime {
 
 
+    public static String isPrime(int num) {
 
-        public static String isPrime(int num) {
+        //Your code goes here
 
-            //Your code goes here
+        if (num <= 1) {
 
-            if(num<=1){
+            return "NO";
+
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+
+            if (num % i == 0) {
 
                 return "NO";
 
             }
 
-            for(int i=2;i<=Math.sqrt(num);i++){
+        }
 
-                if(num%i == 0){
+        return "YES";
 
-                    return "NO";
-
-                }
-
-            }
-
-            return "YES";
-
-         }
+    }
+}
 
 
 
