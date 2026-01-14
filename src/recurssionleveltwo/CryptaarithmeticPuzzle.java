ac735bf14f848553bc9f25d21971fe70d8b9bb97
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-01-14 13:27:30 +0530

    day9

---
+++ src/recurssionleveltwo/CryptaarithmeticPuzzle.java
@@ -0,0 +1,72 @@
+package recurssionleveltwo;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CryptaarithmeticPuzzle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        String s3 = sc.nextLine();
+
+        HashMap<Character,Integer> charMap = new HashMap<>();
+        String unique = "";
+        for(int i = 0;i<s1.length();i++){
+            if(!charMap.containsKey(s1.charAt(i))){
+                charMap.put(s1.charAt(i),-1);
+                unique += s1.charAt(i);
+            }
+        }
+        for(int i = 0;i<s2.length();i++){
+            if(!charMap.containsKey(s2.charAt(i))){
+                charMap.put(s2.charAt(i),-1);
+                unique += s2.charAt(i);
+            }
+        }  for(int i = 0;i<s3.length();i++){
+            if(!charMap.containsKey(s3.charAt(i))){
+                charMap.put(s3.charAt(i),-1);
+                unique += s3.charAt(i);
+            }
+        }
+        boolean[] usednumber = new boolean[10];
+        solution(unique,0,charMap,usednumber,s1,s2,s3);
+    }
+    public static int getNum(String s,HashMap<Character,Integer>charMap){
+        String num = " ";
+        for(int i = 0;i<s.length();i++){
+            num+=charMap.get(s.charAt(i));
+        }
+        return Integer.parseInt(num);
+    }
+    public static void solution(String unique,int idx,HashMap <Character,Integer> charMap,boolean[] usednumber,String s1,String s2,String s3){
+if(idx == unique.length()){
+    int num1 = getNum(s1,charMap);
+    int num2 = getNum(s2,charMap);
+    int num3 =getNum (s3,charMap);
+    if( num1+ num2 == num3){
+        for(int i = 0;i<26;i++){
+            char ch = (char)('a' +i);
+            if(charMap.containsKey(ch)){
+                System.out.println(ch +"-"+charMap.get(ch));
+            }
+        }
+        System.out.println();
+    }
+}
+
+        char ch = unique.charAt(idx);
+for (int num = 0;num<=9;num++){
+    if(usednumber[num] == false){
+        charMap.put(ch,num);
+        usednumber[num] = true;
+        solution(unique,idx + 1,charMap,usednumber,s1,s2,s3);
+        usednumber[num] = false;
+        charMap.put(ch,-1);
+
+
+    }
+}
+    }
+
+}
--- src/recurssionleveltwo/SudukuB.java
+++ src/recurssionleveltwo/SudukuB.java
@@ -1,4 +1,74 @@
 package recurssionleveltwo;
 
+import java.util.Scanner;
+
 public class SudukuB {
+    public static void display(int[][] board){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                System.out.println(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void solveSuduku(int[][] board,int i,int j ){
+        int ni = 0;
+        int nj= 0;
+        if( j == board[0].length -1){
+            ni = i + 1;
+            nj = 0;
+        }
+        else {
+            ni = 0;
+            nj = j + 1;
+        }
+        if(board[i][j] != 0){
+            solveSuduku(board,ni,nj);
+        }else{
+            for (int po = 1;po<=9;po++){
+                if (isValid(board,i,j,po) == true){
+                    board[i][j] = po;
+                    solveSuduku(board,ni,nj);
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+    public static boolean isValid(int [][] board,int x,int y,int po){
+        for(int j = 0;j<board[0].length;j++){
+            if(board[x][j] == po)
+            {
+                return false;
+            }
+        }
+        for(  int i= 0;i<board.length;i++){
+            if(board[i][y] == po)
+            {
+                return false;
+            }
+        }
+   int smi = x/3 * 3;
+   int smj = y/3 * 3;
+   for(int i = 0;i<3;i++){
+       for(int j = 0;j<3;j++){
+           if(board[smi][smj] == po){
+               return false;
+           }
+       }
+   }
+   return true;
+ }
+
+
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int [9][9];
+        for(int i= 0;i<9;i++){
+            for(int j= 0;j<9;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        solveSuduku(arr,0,0);
+    }
 }
