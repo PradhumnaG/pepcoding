16d50343c2722c631b0099216d7ef4b97a1692a1
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-01-15 09:34:09 +0530

    day10

---
+++ src/recurssionleveltwo/PalindromePartitioning.java
@@ -0,0 +1,65 @@
+package recurssionleveltwo;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+//all character be even
+//all character be even and only one odd. 
+public class PalindromePartitioning {
+    public static void generatepw(int cs,int ts,HashMap<Character,Integer>fmap,Character oddc,String asf){
+       if(cs>ts){
+           String rev = "";
+           for (int i = asf.length()-1;i>=0;i--){
+               rev +=asf.charAt(i);
+           }
+           String res = asf;
+           if(oddc !=null){
+               res +=oddc;
+           }
+           res +=rev;
+           System.out.println(res);
+           return;
+       }
+
+        for(char ch : fmap.keySet()){
+            int freq = fmap.get(ch);
+            if(freq > 0){
+                fmap.put(ch,freq -1);
+                generatepw(cs+1,ts,fmap,oddc,asf +ch);
+                fmap.put(ch,freq +1);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        HashMap<Character,Integer> fmap = new HashMap<>();
+        for(int i = 0;i <str.length();i++){
+            char ch = str.charAt(i);
+            if(fmap.containsKey(ch)){
+                int of = fmap.get(ch);
+                fmap.put(ch,of +1);
+            }else{
+                fmap.put(ch,1);
+
+            }
+           /* fmap.put(ch,fmap.getOrDefault(ch,0)+1);*/
+        }
+        Character odd = null;
+        int odds = 0;
+        int len = 0;
+        for (char ch: fmap.keySet()){
+            int freq = fmap.get(ch);
+            if(freq % 2 == 1){
+                odd = ch ;
+                odds++;
+            }
+            fmap.put(ch,freq/2);
+            len +=freq/2;
+        }
+    generatepw(1,len,fmap,odd,"");
+    }
+
+
+}
--- src/recurssionleveltwo/PartitioninKSubsets.java
+++ src/recurssionleveltwo/PartitioninKSubsets.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class PartitioninKSubsets {
    static int count = 1;
     public static void  solution(int i, int n, int k, int asf, ArrayList<ArrayList<Integer>> ans){
-if(i >4){
-    if(asf == k){
+if(i >n){ // check till i is greater then n
+    if(asf == k){//check no of set are eqal to no of sets
         count++;
         System.out.println(count+"");
         for(ArrayList<Integer> set : ans){
@@ -22,7 +22,7 @@ if(i >4){
     if (ans.get(j).size() >0){
         ans.get(j).add(i);
         solution(i,n,k,asf,ans);
-        ans.get(j).remove(ans.get(j).size() - 1);
+        ans.get(j).remove(ans.get(j).size() - 1);//remove the vale during backtracking
     }else{
         ans.get(j).add(i);
         solution(i,n,k,asf +1,ans);
