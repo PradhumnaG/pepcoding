1c543a7e4d71ba3779bdc24dbd4dcdb4cb579a44
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-02-09 23:48:52 +0530

    day39

--- src/day2/Bufferreader.java
+++ src/day2/Bufferreader.java
@@ -12,7 +12,7 @@ public class Bufferreader {
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-        }
+        }//
         int [] iarr = allIndices(arr, x, 0, 0);
         if(iarr.length == 0){
             System.out.println();
