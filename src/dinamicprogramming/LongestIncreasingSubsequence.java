99c2bb601d5e8c734d15c4100184ea22e2806767
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-03-01 20:46:35 +0530

    day58

---
+++ src/dinamicprogramming/LongestIncreasingSubsequence.java
@@ -0,0 +1,36 @@
+package dinamicprogramming;
+
+public class LongestIncreasingSubsequence  {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+
+        int overallMax = 0;
+
+
+        for (int i = 0; i < n; i++) {
+            int max = 0;
+
+
+            for (int j = 0; j < i; j++) {
+
+                if (nums[j] < nums[i]) {
+                    if (dp[j] > max) {
+                        max = dp[j];
+                    }
+                }
+            }
+
+
+            dp[i] = max + 1;
+
+
+            if (dp[i] > overallMax) {
+                overallMax = dp[i];
+            }
+        }
+        return overallMax;
+
+    }
+}
